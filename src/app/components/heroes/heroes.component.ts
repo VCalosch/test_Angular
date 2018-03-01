3dc72fc5abc941b540a88bd6f6f2d98b374d8f18
VCalosch <carlos.villanueva@alumnos.uneatlantico.es>
2018-03-01 01:50:19 +0100

    Initial commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+
+  constructor(private _HeroesService:HeroesService, private _Router:Router) { }
+
+  ngOnInit() {
+
+    this.heroes = this._HeroesService.getHeroes();
+    console.log(this.heroes);
+
+  }
+
+  verHeroe( index:number ){
+    this._Router.navigate(['/heroe', index]);
+  }
+}
